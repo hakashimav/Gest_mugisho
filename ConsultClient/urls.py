9ef3d92f9aff945ec631442968aa44b9ae323499
MichMav <michmav28@gmail.com>
2024-07-17 23:45:38 +0100

    add the app dash

--- ConsultClient/urls.py
+++ ConsultClient/urls.py
@@ -20,6 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path("", include("WebApp.urls")),
+    path('', include("WebApp.urls")),
+    path('Dashboard/', include("Dash.urls")),
     path('admin/', admin.site.urls),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
