ad3f36b7552581ab185506a09b471372ae4e0203
MichMav <michmav28@gmail.com>
2024-08-08 16:38:49 +0100

    fx the all files

--- App/views.py
+++ App/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
+def index(request):
+    context = {}
+    template = loader.get_template('index2.html')
+    return HttpResponse(template.render(context, request))
+
+
+def datatable(request):
+    context = {}
+    template = loader.get_template('datatable.html')
+    return HttpResponse(template.render(context, request))
+
+
+
+def forms(request):
+    context = {}
+    template = loader.get_template('forms.html')
+    return HttpResponse(template.render(context, request))
