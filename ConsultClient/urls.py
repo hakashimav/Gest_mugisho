4c019771885ec26a8453268d84ef724a086e4b52
MichMav <michmav28@gmail.com>
2024-09-10 23:02:33 +0100

    fx the dash

--- ConsultClient/urls.py
+++ ConsultClient/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', include("WebApp.urls")),
-    path('Dashboard/', include("App.urls")),
+    # path('', include("WebApp.urls")),
+    path('', include("App.urls")),
     path('admin/', admin.site.urls),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
